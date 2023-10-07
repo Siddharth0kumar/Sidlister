c8c12ad1fd9b314ab0e81ca48f337081c7b70f45
Siddharth0kumar <89460704+Siddharth0kumar@users.noreply.github.com>
2023-10-07 12:25:10 +0000

    # A Setup Bash File

---
+++ sidup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install sublist3r using pip
+pip install sublist3r
+
+# Install pyfiglet
+pip install pyfiglet
+
+# Install cowsay and fortune
+sudo apt-get update
+sudo apt-get install cowsay fortune -y
+
+echo -e "\e[1;32mSetup completed. You can now run your Sidlister Tool.\e[0m"
+
